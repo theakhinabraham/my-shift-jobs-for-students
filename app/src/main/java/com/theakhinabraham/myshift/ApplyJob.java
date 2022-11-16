152282ae2b503fb40ba27e6afbff495a3dc2bf20
Akhin Abraham <theakhinabraham@gmail.com>
2022-11-16 13:28:00 +0530

    Basic Button functionality

--- app/src/main/java/com/theakhinabraham/myshift/ApplyJob.java
+++ app/src/main/java/com/theakhinabraham/myshift/ApplyJob.java
@@ -2,13 +2,38 @@ package com.theakhinabraham.myshift;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ApplyJob extends AppCompatActivity {
+
+    EditText role, description, salary, locality, requirements;
+    Button applyJobBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_job);
+        
+        role = findViewById(R.id.role);
+        description = findViewById(R.id.description);
+        salary = findViewById(R.id.salary);
+        locality = findViewById(R.id.locality);
+        requirements = findViewById(R.id.requirements);
+        applyJobBtn = findViewById(R.id.applyJobBtn);
+        
+        applyJobBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ApplyJob.this, StudentHome.class);
+                startActivity(intent);
+                Toast.makeText(ApplyJob.this, "Applied for Job!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
     }
 }
