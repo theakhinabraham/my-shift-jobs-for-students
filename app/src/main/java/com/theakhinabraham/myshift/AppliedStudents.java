6769f47d19bcc03a0d9d8dda0ecd63821536cd41
Akhin Abraham <theakhinabraham@gmail.com>
2023-02-10 11:46:15 +0530

    TODO: Application Processes

--- app/src/main/java/com/theakhinabraham/myshift/AppliedStudents.java
+++ app/src/main/java/com/theakhinabraham/myshift/AppliedStudents.java
@@ -1,12 +1,17 @@
 package com.theakhinabraham.myshift;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AppliedStudents extends AppCompatActivity {
 
-
+//TODO: VIEW APPLIED STUDENTS RESPECTIVE TO JOB POSTED (FOR COMPANY)
+    //1. DISPLAY COMPANY USER JOBS ONLY
+    //2. CLICKABLE RECYCLER VIEW -> VIEW APPLICANTS
+    //3. APPROVE/REJECT APPLICANT
+    //4. UPDATE APPLIED DB
+    //5. DELETE POSTING
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/theakhinabraham/myshift/ViewApplication.java
@@ -0,0 +1,16 @@
+package com.theakhinabraham.myshift;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewApplication extends AppCompatActivity {
+
+    //TODO: VIEW ALL APPLIED ROLES AND STATUS (FOR STUDENTS)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_application);
+    }
+}
